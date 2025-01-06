acc6a675b2c55ddc2ce5666e3d21d01edf38ae9c
BHR_VERME522@kpn.com <BHR_VERME522@kpn.com>
2025-01-06 20:09:23 +0100

    .

--- metro/adf/Views/vw_DWH_Date.sql
+++ metro/adf/Views/vw_DWH_Date.sql
@@ -1,17 +1,28 @@
-CREATE VIEW [adf].[vw_DWH_Date] AS
-/*
+﻿
+
+
+
+
+
+
+
+
+CREATE view [adf].[vw_DWH_Date] as
+
+/* 
 Description:
-    Date Table from 2010-01-01 till 2039-12-31
+	Date Table from 2010-01-01 till 2039-12-31
+
+
 */
 
-SELECT
-    src.*, 
-    RepositoryStatusName = SDTAP.RepositoryStatus,
-    RepositoryStatusCode = SDTAP.RepositoryStatusCode,
-    Environment          = SDTAP.RepositoryStatus
-FROM 
-    [adf].[DWH_Date] src
-CROSS JOIN 
-    [adf].[vw_SDTAP] SDTAP
-WHERE 
-    SDTAP.RepositoryStatusCode > -2;
+select
+	 src.* 
+
+	, RepositoryStatusName	= SDTAP.RepositoryStatus
+	, RepositoryStatusCode	= SDTAP.RepositoryStatusCode 
+	, Environment			= SDTAP.RepositoryStatus
+from  [adf].[DWH_Date]   src
+Cross join [adf].[vw_SDTAP] SDTAP
+where 1=1
+and SDTAP.RepositoryStatusCode > -2
