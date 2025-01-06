593c656b7e484849a9cc2bbb6a0882ae4348fb89
Kim Vermeij <33691406+kimv001@users.noreply.github.com>
2025-01-06 19:53:33 +0100

    Update vw_DWH_Time.sql

--- metro/adf/Views/vw_DWH_Time.sql
+++ metro/adf/Views/vw_DWH_Time.sql
@@ -1,25 +1,20 @@
-﻿
-
-
-
-
-CREATE view [adf].[vw_DWH_Time] as
-
-/* 
+CREATE VIEW [adf].[vw_DWH_Time] AS
+/*
 Description:
-	Time Table in HH:MM
+    Time Table in HH:MM
 
 *Note
-	SS and MS came along, but always 0
+    SS and MS came along, but always set 0
 */
 
-select
-	src.* 
-
-	, RepositoryStatusName	= SDTAP.RepositoryStatus
-	, RepositoryStatusCode	= SDTAP.RepositoryStatusCode 
-	, Environment			= SDTAP.RepositoryStatus
-from  [adf].[DWH_Time]   src
-Cross join [adf].[vw_SDTAP] SDTAP
-where 1=1
-and SDTAP.RepositoryStatusCode > -2
+SELECT
+    src.*, 
+    RepositoryStatusName = SDTAP.RepositoryStatus,
+    RepositoryStatusCode = SDTAP.RepositoryStatusCode,
+    Environment          = SDTAP.RepositoryStatus
+FROM 
+    [adf].[DWH_Time] src
+CROSS JOIN 
+    [adf].[vw_SDTAP] SDTAP
+WHERE 
+    SDTAP.RepositoryStatusCode > -2;
