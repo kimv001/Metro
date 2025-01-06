6d22f490825c5617bf4fc158ceceb97b998c6a07
Kim Vermeij <33691406+kimv001@users.noreply.github.com>
2025-01-06 19:54:24 +0100

    Update vw_DataSourceProperties.sql

--- metro/adf/Views/vw_DataSourceProperties.sql
+++ metro/adf/Views/vw_DataSourceProperties.sql
@@ -1,54 +1,52 @@
-﻿
-Create view [adf].[vw_DataSourceProperties] as
-
+CREATE VIEW [adf].[vw_DataSourceProperties] AS
 /*
-Developed by:			metro
-Description:			Generic transformationview to pivot all DataSourceProperties to DTAP environment values
+Developed by:            metro
+Description:             Generic transformation view to pivot all DataSourceProperties to DTAP environment values
 
 Change log:
-Date					Author				Description
-20220915 00:00			K. Vermeij			Initial version
+Date                     Author                Description
+20220915 00:00           K. Vermeij            Initial version
 */
 
-select 
-	  BK_Datasource
-	, DataSourceName
-	, BK_LinkedService
-	, DataSourcePropertiesName
-	, CurrentEnvironment					= CASE WHEN right(CAST(SERVERPROPERTY( 'ServerName' ) as varchar),3)='ROD' THEN 'PRD' ELSE right(CAST(SERVERPROPERTY( 'ServerName' ) as varchar),3) END
-	, DataSourcePropertiesCurrentValue		= CASE 
-												WHEN right(CAST(SERVERPROPERTY( 'ServerName' ) as varchar),3)='dev' then ISNULL([D],[X])
-												WHEN right(CAST(SERVERPROPERTY( 'ServerName' ) as varchar),3)='tst' then ISNULL([T],[X])
-												WHEN right(CAST(SERVERPROPERTY( 'ServerName' ) as varchar),3)='acc' then ISNULL([A],[X])
-												WHEN right(CAST(SERVERPROPERTY( 'ServerName' ) as varchar),3)='rod' then ISNULL([P],[X])
-												WHEN right(CAST(SERVERPROPERTY( 'ServerName' ) as varchar),3)='box' then ISNULL([S],[X])
-												ELSE 'Unknown'
-												END
-	
-
-	, D = ISNULL([D],[X])
-	, T = ISNULL([T],[X])
-	, A = ISNULL([A],[X])
-	, P = ISNULL([P],[X])
-	, X = [X] -- default value for all DTAP
-	, S = ISNULL([S],[X]) -- Sandbox environment
-	
-	from (
-
-			Select 
-				BK_Datasource						= ds.bk
-				, BK_LinkedService					= ds.BK_LinkedService
-				, DataSourceName					= ds.[Name]
-				, DataSourcePropertiesName			= src.DataSourcePropertiesName
-				, DataSourcePropertiesValue			= src.DataSourcePropertiesValue
-				, DataSourcePropertiesEnvironment	= rt.code
-			  From  rep.vw_DataSource				ds
-			  join rep.vw_DataSourceProperties		src	on src.BK_Datasource	= ds.bk
-			  join rep.vw_refType					rt	on rt.bk				= src.bk_RefType_Environment
-			  ) dsp
-Pivot (
-									max([DataSourcePropertiesValue])
-							
-for [DataSourcePropertiesEnvironment] IN 
-			([D],[T],[A],[P],[X], [S])
-		)	as pivot_table
+SELECT 
+      BK_Datasource,
+      DataSourceName,
+      BK_LinkedService,
+      DataSourcePropertiesName,
+      CurrentEnvironment = CASE 
+          WHEN RIGHT(CAST(SERVERPROPERTY('ServerName') AS VARCHAR), 3) = 'ROD' THEN 'PRD' 
+          ELSE RIGHT(CAST(SERVERPROPERTY('ServerName') AS VARCHAR), 3) 
+      END,
+      DataSourcePropertiesCurrentValue = CASE 
+          WHEN RIGHT(CAST(SERVERPROPERTY('ServerName') AS VARCHAR), 3) = 'dev' THEN ISNULL([D], [X])
+          WHEN RIGHT(CAST(SERVERPROPERTY('ServerName') AS VARCHAR), 3) = 'tst' THEN ISNULL([T], [X])
+          WHEN RIGHT(CAST(SERVERPROPERTY('ServerName') AS VARCHAR), 3) = 'acc' THEN ISNULL([A], [X])
+          WHEN RIGHT(CAST(SERVERPROPERTY('ServerName') AS VARCHAR), 3) = 'rod' THEN ISNULL([P], [X])
+          WHEN RIGHT(CAST(SERVERPROPERTY('ServerName') AS VARCHAR), 3) = 'box' THEN ISNULL([S], [X])
+          ELSE 'Unknown'
+      END,
+      D = ISNULL([D], [X]),
+      T = ISNULL([T], [X]),
+      A = ISNULL([A], [X]),
+      P = ISNULL([P], [X]),
+      X = [X], -- default value for all DTAP
+      S = ISNULL([S], [X]) -- Sandbox environment
+FROM (
+    SELECT 
+          BK_Datasource = ds.bk,
+          BK_LinkedService = ds.BK_LinkedService,
+          DataSourceName = ds.[Name],
+          DataSourcePropertiesName = src.DataSourcePropertiesName,
+          DataSourcePropertiesValue = src.DataSourcePropertiesValue,
+          DataSourcePropertiesEnvironment = rt.code
+    FROM  
+          rep.vw_DataSource ds
+    JOIN 
+          rep.vw_DataSourceProperties src ON src.BK_Datasource = ds.bk
+    JOIN 
+          rep.vw_refType rt ON rt.bk = src.bk_RefType_Environment
+    ) dsp
+PIVOT (
+    MAX([DataSourcePropertiesValue])
+    FOR [DataSourcePropertiesEnvironment] IN ([D], [T], [A], [P], [X], [S])
+) AS pivot_table;
