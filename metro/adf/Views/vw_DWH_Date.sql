9399afce25818af2880b2709eb9c93ae2efeaf25
Kim Vermeij <33691406+kimv001@users.noreply.github.com>
2025-01-06 19:52:41 +0100

    Update vw_DWH_Date.sql

--- metro/adf/Views/vw_DWH_Date.sql
+++ metro/adf/Views/vw_DWH_Date.sql
@@ -1,28 +1,17 @@
-﻿
-
-
-
-
-
-
-
-
-CREATE view [adf].[vw_DWH_Date] as
-
-/* 
+CREATE VIEW [adf].[vw_DWH_Date] AS
+/*
 Description:
-	Date Table from 2010-01-01 till 2039-12-31
-
-
+    Date Table from 2010-01-01 till 2039-12-31
 */
 
-select
-	 src.* 
-
-	, RepositoryStatusName	= SDTAP.RepositoryStatus
-	, RepositoryStatusCode	= SDTAP.RepositoryStatusCode 
-	, Environment			= SDTAP.RepositoryStatus
-from  [adf].[DWH_Date]   src
-Cross join [adf].[vw_SDTAP] SDTAP
-where 1=1
-and SDTAP.RepositoryStatusCode > -2
+SELECT
+    src.*, 
+    RepositoryStatusName = SDTAP.RepositoryStatus,
+    RepositoryStatusCode = SDTAP.RepositoryStatusCode,
+    Environment          = SDTAP.RepositoryStatus
+FROM 
+    [adf].[DWH_Date] src
+CROSS JOIN 
+    [adf].[vw_SDTAP] SDTAP
+WHERE 
+    SDTAP.RepositoryStatusCode > -2;
